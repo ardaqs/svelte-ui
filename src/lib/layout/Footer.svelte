a52b6735de00af983ca9a566b6ce5518e38c3bcc
Ardak Saduakassov <ardak.saduakassov@bsbnb.kz>
2023-04-19 17:51:49 +0600

    Footer component

---
+++ src/lib/layout/Footer.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  $: currYear = (new Date()).getFullYear().toString();
+</script>
+
+<footer>
+  <section class="centered copyright">
+    <p>© {currYear}&nbsp;&nbsp;<slot name="copyright"/></p>
+  </section>
+</footer>
+
+<style>
+  section.centered {
+    text-align: center;
+    vertical-align: middle;
+  }
+  section.copyright p {
+    margin: 0.5rem;
+    font-size: small;
+  }
+</style>
